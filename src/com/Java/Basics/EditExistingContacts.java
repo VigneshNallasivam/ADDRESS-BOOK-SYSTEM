59d64c223f492eea5b30a8fcf19f1d70153b1e77
VigneshNallasivam <vickyshanestark@gmail.com>
2022-12-01 20:17:20 +0530

    Added : UC-3 Using Scanners & ArrayList for Editing Existing Contacts

---
+++ src/com/Java/Basics/EditExistingContacts.java
@@ -0,0 +1,82 @@
+package com.Java.Basics;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class EditExistingContacts 
+{
+	Scanner scanner = new Scanner(System.in);
+    AddNewContactsInAddressBook contact = new AddNewContactsInAddressBook();
+	ArrayList<AddNewContactsInAddressBook> contactsArrayList = new ArrayList<AddNewContactsInAddressBook>();
+    /*
+     * Using Scanners & ArrayList for Editing Existing Contacts
+     */
+	public void addContact() 
+	{
+		AddNewContactsInAddressBook contact = new AddNewContactsInAddressBook();
+        System.out.println("Enter your FirstName: ");
+		contact.setFirstName(scanner.next());
+		System.out.println("Enter your LastName: ");
+		contact.setLastName(scanner.next());
+		System.out.println("Enter your Email: ");
+		contact.setEmail(scanner.next());
+		System.out.println("Enter your Address: ");
+		contact.setAddress(scanner.next());
+		System.out.println("Enter your City: ");
+		contact.setCity(scanner.next());
+		System.out.println("Enter your State: ");
+		contact.setState(scanner.next());
+		System.out.println("Enter your Zipcode: ");
+		contact.setZip(scanner.nextInt());
+		System.out.println("Enter your Mobile Number: ");
+		contact.setMobile(scanner.nextLong());
+        contactsArrayList.add(contact);
+	}
+
+	public void showContacts() 
+	{
+		System.out.println(contact.toString());
+		for (int i = 0; i < contactsArrayList.size(); i++) 
+		{
+			AddNewContactsInAddressBook contacts = contactsArrayList.get(i);
+			System.out.println(contacts.toString());
+		}
+
+	}
+
+	public void editContact()
+	{
+
+		System.out.println("Enter the First Name to Edit : ");
+		String FirstName = (scanner.next());
+		boolean IsAvaible = false;
+
+		for (AddNewContactsInAddressBook contact : contactsArrayList)
+		{
+			if (contact.getFirstName().equals(FirstName))
+			{
+				IsAvaible = true;
+
+				System.out.println("Enter your FirstName: ");
+				contact.setFirstName(scanner.next());
+				System.out.println("Enter your LastName: ");
+				contact.setLastName(scanner.next());
+				System.out.println("Enter your Email: ");
+				contact.setEmail(scanner.next());
+				System.out.println("Enter your Address: ");
+				contact.setAddress(scanner.next());
+				System.out.println("Enter your City: ");
+				contact.setCity(scanner.next());
+				System.out.println("Enter your State: ");
+				contact.setState(scanner.next());
+				System.out.println("Enter your Zipcode: ");
+				contact.setZip(scanner.nextInt());
+				System.out.println("Enter your Mobile Number: ");
+				contact.setMobile(scanner.nextLong());
+			}
+		}
+
+		if (IsAvaible == false)
+		{
+			System.out.println("Contact Doesn't exist.");
+		}
+	}
+}
