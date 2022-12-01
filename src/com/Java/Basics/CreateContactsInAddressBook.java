1568f33b04e071af71d4057e666ad36b3ce2abcb
VigneshNallasivam <vickyshanestark@gmail.com>
2022-12-01 20:02:00 +0530

    Added : UC-1 Creating the contact details for address in Constructor

---
+++ src/com/Java/Basics/CreateContactsInAddressBook.java
@@ -0,0 +1,29 @@
+package com.Java.Basics;
+public class CreateContactsInAddressBook 
+{
+	private String firstName, lastName, email, address, city, state;
+	private int zip;
+	private long mobile;
+	/*
+	 * Creating the contact details for address in Constructor
+	 */
+	public CreateContactsInAddressBook(String firstName, String lastName, String email, String address, String city, String state,
+			int zip, long mobile) 
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.mobile = mobile;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "ContactPerson [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", address="
+				+ address + ", city=" + city + ", state=" + state + ", zip=" + zip + ", mobile=" + mobile + "]";
+	}
+}
