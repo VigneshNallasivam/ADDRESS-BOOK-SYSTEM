1869f82fe8f0cd68b6b4367e931b9d5c9191be4d
VigneshNallasivam <vickyshanestark@gmail.com>
2023-01-04 08:58:37 +0530

    Added : UC-14 Adding File Reading & Writing in CSV File

---
+++ src/com/Java/Basics/FileIOCsv.java
@@ -0,0 +1,46 @@
+package com.Java.Basics;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+public class FileIOCsv 
+{
+	/*
+	 * METHOD FOR DOING WRITE OPERATION 
+	 */
+	public static void write() throws IOException
+	{
+		StreamCity obj = new StreamCity();
+		String str = obj.toString();
+		String text= String.valueOf(obj);
+		StringBuffer buffer = new StringBuffer(" Hai ");
+		buffer.append(text);
+		Path fileName = Path.of("C:\\Users\\lab\\Desktop\\JAVA\\JAVA Assignments\\ADDRESS BOOK SYSTEM\\Address Book System Program\\src\\com\\Java\\Basics\\message.csv");
+		Files.writeString(fileName, buffer);
+	}
+	/*
+	 * METHOD FOR DOING READ OPERATION 
+	 */
+	public static void read() throws IOException
+	{
+		File file = new File("C:\\\\Users\\\\lab\\\\Desktop\\\\JAVA\\\\JAVA Assignments\\\\ADDRESS BOOK SYSTEM\\\\Address Book System Program\\\\src\\\\com\\\\Java\\\\Basics\\\\message.csv");
+		BufferedReader br= new BufferedReader(new FileReader(file));
+		String st;
+		while ((st = br.readLine()) != null)
+		{
+			System.out.println(st);
+		}
+	}
+	/*
+	 * MAIN METHOD OR DRIVER METHOD 
+	 */
+	public static void main(String[] args) throws IOException
+	{
+		write();
+		read();
+	}
+
+}
